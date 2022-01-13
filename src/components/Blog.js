4368f007e5faa2ef2ac1325cb99b9d55d88063a0
Chris Williams <christopher.lee.williams@gmail.com>
2022-01-13 17:12:34 -0600

    Completing 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,29 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+
+const Blog = ({blog}) => {
+  const [detailView, setDetailView] = useState(false)
+
+  const hideWhenDetail = { display: detailView ? 'none' : '' }
+  const showWhenDetail = { display: detailView ? '' : 'none' }
+
+  const toggleDetail = () => {
+    setDetailView(!detailView)
+  }
+  
+  return (
+    <div>
+      <div className="blog-style" style={hideWhenDetail}>
+        {blog.title} {blog.author} <button onClick={toggleDetail}>View</button>
+      </div> 
+      <div className="blog-style" style={showWhenDetail}>
+        {blog.title} {blog.author} <button onClick={toggleDetail}>Hide</button>
+        <br/>
+        {blog.url} <br/>
+        likes {blog.likes} <button>like</button><br/>
+        {blog.user.username}
+      </div> 
+    </div> 
+  )
+}
 
 export default Blog
