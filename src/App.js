3c48ecdfb08f40e7e31d8374e5b4f61be61acb98
Chris Williams <christopher.lee.williams@gmail.com>
2022-01-13 11:04:36 -0600

    update formatting

--- src/App.js
+++ src/App.js
@@ -63,10 +63,15 @@ const App = () => {
   return (
     <div>   
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-      <button onClick={handleLogout}>Logout</button>
+      <div>
+        {user.username} logged in 
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+      <div>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
     </div>
   )
 }
